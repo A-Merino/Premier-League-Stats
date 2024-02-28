41f7d86b4fe149d4aa1d9e0d2a127b30989c7d33
Alex-M22 <alexquiquemerino@gmail.com>
2024-02-28 13:01:31 -0500

    2-28

--- tables.py
+++ tables.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def table_cleaner(table):
+    ex = ["Playing","Standard" , "Goals", "Total", "Pass", "SCA", "Tackles", "Touches", "Performance"]
     with open("tab.csv", "w") as file:
         writ = csv.writer(file, delimiter=",")
 
@@ -12,7 +13,7 @@ def table_cleaner(table):
             line = line.split(" ")
             line.remove(line[-1])
 
-            if line[0] == "Playing":
+            if line[0] in ex:
                 continue
             elif line[0] != "Player":
                 line[0] = " ".join((line[0], line[1]))
@@ -20,11 +21,22 @@ def table_cleaner(table):
                 line.remove(line[1])
             elif i != 1:
                 continue
-
+            # print(line)
             line.remove(line[1])
+            while "Pen" in line:
+                line.remove("Pen")
+            while "3rd" in line:
+                line.remove("3rd")
+            while "(GK)" in line:
+                line.remove("(GK)")
             writ.writerow(line)
 
 
 def table_joiner(df):
-    table = pd.read_csv("tab.csv")
-    print(table)
+    table = pd.read_csv("tab.csv", encoding="latin1")
+    table = table.iloc[:-2,:]
+    if df.empty:
+        return table
+    return pd.concat([table, df])
+    # return pd.merge(table, df, how="left")
+
